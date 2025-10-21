a5561052609d7fd8de70c8a61cbc72a6f93222ae
Julian16124567 <julian.zotter.01@icloud.com>
2025-10-21 22:43:09 +0200

    Add core scripts for Acheron2

---
+++ src/functions.py
@@ -0,0 +1,60 @@
+import sys
+import time
+
+asciiName = r"""
+
+                 ___        __                        
+                /   | _____/ /_  ___  _________  ____ 
+               / /| |/ ___/ __ \/ _ \/ ___/ __ \/ __ \
+              / ___ / /__/ / / /  __/ /  / /_/ / / / /
+             /_/  |_\___/_/ /_/\___/_/   \____/_/ /_/ 
+           <=========================================>
+
+"""
+
+def drawMain():
+    print("\033[95m" + asciiName + "\033[0m")
+    print("\n\n")
+
+def animateText(text, amount):
+    loading = ["", ".", "..", "..."] 
+    color = "\033[95m"  
+    reset = "\033[0m"
+    for i in range(amount):
+        for frame in loading:
+            sys.stdout.write(f"\r{color}[*] {text}{frame}{reset}   ")
+            sys.stdout.flush()
+            time.sleep(0.4)
+    print()
+
+def checkEmail():
+    with open("email.txt", "w") as file:
+        inhalt = file.read()
+        if len(inhalt) == 0:
+            file.close()
+            return False
+        else:
+            return True
+
+def checkPwd():
+    with open("pwd.txt", "w") as file:
+        inhalt = file.read()
+        if len(inhalt) == 0:
+            file.close()
+            return False;
+        else:
+            return True;
+
+def writeCredentials(file_name, inp_file):
+    with open(f"{file_name}", "w") as file:
+        file.write(inp_file)
+        file.close()
+
+def getCredentials(text):
+    text = input("\033[36m[*] {text}:\n\033[0m").strip()
+    return text
+
+def writeError(e):
+    with open("../errorlog/log.txt", "w") as file:
+        message = f"[*] {e}"
+        file.write(message)
