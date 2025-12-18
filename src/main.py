0ebde6718aeb4ab3460fd74ffd486e1afc995050
Julian <julian.zotter.01@icloud.com>
2025-12-18 13:26:07 +0100

    main.py aktualisieren

--- src/main.py
+++ src/main.py
@@ -84,7 +84,7 @@ sys.stdout.flush()
 try:
     header = functions.getHeader()
     text = functions.getText()    
-    mailto = "julian.zotter.01@icloud.com"                 
+    mailto = functions.getRequest()                
     amountmails = functions.getAmount()
     me = functions.mailFrom()
     pwd = functions.getPwd()
